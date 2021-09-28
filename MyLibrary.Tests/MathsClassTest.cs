8c22d02b5825fd9877519a8c1f81e37ede0b2944
Thibaut Montanier <thibaut.montanier@gmail.com>
2021-09-28 08:50:57 +0200

    Ajoutez des fichiers projet.

---
+++ MyLibrary.Tests/MathsClassTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyLibrary.Tests {
+    class MathsClassTest {
+        private MathsClass mathscls;
+
+        [SetUp]
+        public void Setup() {
+            this.mathscls = new MathsClass();
+        }
+        [Test]
+        public void Add3p4Test() {
+            int result = mathscls.Add(3, 4);
+            Assert.AreEqual(7, result);
+
+        }
+    }
+}
---
+++ MyLibrary/MathsClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyLibrary {
+    public class MathsClass {
+
+        public int Add(int nb1, int nb2) {
+            return nb1 + nb2;
+        }
+    }
+}
